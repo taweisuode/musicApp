3c55bc7b410c6d5f124eb25c54c2513b8e09c3e8
Pengge Zhuang <1064480036@qq.com>
2017-09-30 18:11:12 +0800

    add:播放界面

--- app/src/main/java/com/pengge/musicplayer/musicModel/ParseLrc.java
+++ app/src/main/java/com/pengge/musicplayer/musicModel/ParseLrc.java
@@ -3,6 +3,7 @@ package com.pengge.musicplayer.musicModel;
 import com.pengge.musicplayer.tools.ShowLog;
 
 import java.io.IOException;
+import java.util.TreeMap;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -16,17 +17,17 @@ public class ParseLrc {
     private String lrc;
     private OkHttpClient client = null;
 
-    public ParseLrc(String lrc) {
-        this.lrc = lrc;
+    public ParseLrc(String lrcUrl) {
         client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(lrc)
+                .url(lrcUrl)
                 .build();
 
         try {
             Response response = client.newCall(request).execute();
             String result = response.body().string();
             ShowLog.e(result);
+            this.lrc = result;
         } catch (IOException e) {
             e.printStackTrace();
         }
